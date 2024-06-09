d15dc763fd445c6b0cc94794e4e27c3933727a0b
uncpleo <leonardocqtc@gmail.com>
2024-06-08 22:35:30 -0500

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import("./shared/components/layout/layout.component"),
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import("./business/dashboard/dashboard.component")
+            },
+            {
+                path: 'profile',
+                loadComponent: () => import("./business/profile/profile.component")
+            },
+            {
+                path: 'tablas',
+                loadComponent: () => import("./business/tablas/tablas.component")
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
